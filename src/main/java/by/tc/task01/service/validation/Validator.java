9ab9c521ef8278e89c0d18876dbc02c1cff50877
JustDalo <shishlodan@mail.ru>
2021-11-24 11:20:56 +0300

    validator for other appliances

--- src/main/java/by/tc/task01/service/validation/Validator.java
+++ src/main/java/by/tc/task01/service/validation/Validator.java
@@ -11,6 +11,24 @@ public class Validator {
 			case "Kettle" -> {
 				return KettleValidator(criteria);
 			}
+			case "Laptop" -> {
+				return LaptopValidator(criteria);
+			}
+			case "Oven" -> {
+				return OvenValidator(criteria);
+			}
+			case "Refrigerator" -> {
+				return RefrigeratorValidator(criteria);
+			}
+			case "Speakers" -> {
+				return SpeakersValidator(criteria);
+			}
+			case "TabletPC" -> {
+				return TabletPCValidator(criteria);
+			}
+			case "VacuumCleaner" -> {
+				return VacuumCleanerValidator(criteria);
+			}
 		}
 
 		return true;
@@ -23,16 +41,140 @@ public class Validator {
 				case "price", "powerConsumption", "weight", "capacity" -> {
 					if (!criteria.getCriteria().get(property).getClass().getName().contains("Integer")){
 						return true;
-					};
+					}
 				}
 				default -> {
-					return true;
+					return false;
 				}
 			}
 		}
 		return false;
 	}
 
-}
+	private static boolean LaptopValidator(Criteria criteria){
+		Set<String> applianceProperties = criteria.getCriteria().keySet();
+		for (String property: applianceProperties) {
+			switch (property) {
+				case "price", "powerConsumption", "weight", "capacity" -> {
+					if (!criteria.getCriteria().get(property).getClass().getName().contains("Integer")){
+						return true;
+					}
+				}
+				case "CPU", "batteryCapacity" -> {
+					if (!criteria.getCriteria().get(property).getClass().getName().contains("Float")){
+						return true;
+					}
+				}
+				default -> {
+					return false;
+				}
+			}
+		}
+		return false;
+	}
 
-//you may add your own new classes
+	private static boolean OvenValidator(Criteria criteria){
+		Set<String> applianceProperties = criteria.getCriteria().keySet();
+		for (String property: applianceProperties) {
+			switch (property) {
+				case "price", "powerConsumption", "weight", "capacity" -> {
+					if (!criteria.getCriteria().get(property).getClass().getName().contains("Integer")){
+						return true;
+					}
+				}
+				case "width", "height" -> {
+					if (!criteria.getCriteria().get(property).getClass().getName().contains("Float")){
+						return true;
+					}
+				}
+				default -> {
+					return false;
+				}
+			}
+		}
+		return false;
+	}
+
+	private static boolean RefrigeratorValidator(Criteria criteria){
+		Set<String> applianceProperties = criteria.getCriteria().keySet();
+		for (String property: applianceProperties) {
+			switch (property) {
+				case "price", "powerConsumption", "weight", "freezerCapacity", "overallCapacity" -> {
+					if (!criteria.getCriteria().get(property).getClass().getName().contains("Integer")){
+						return true;
+					}
+				}
+				case "width", "height" -> {
+					if (!criteria.getCriteria().get(property).getClass().getName().contains("Float")){
+						return true;
+					}
+				}
+				default -> {
+					return false;
+				}
+			}
+		}
+		return false;
+	}
+
+	private static boolean SpeakersValidator(Criteria criteria){
+		Set<String> applianceProperties = criteria.getCriteria().keySet();
+		for (String property: applianceProperties) {
+			switch (property) {
+				case "price", "powerConsumption", "numberOfSpeakers", "cordLength" -> {
+					if (!criteria.getCriteria().get(property).getClass().getName().contains("Integer")){
+						return true;
+					}
+				}
+				default -> {
+					return false;
+				}
+			}
+		}
+		return false;
+	}
+
+	private static boolean TabletPCValidator(Criteria criteria){
+		Set<String> applianceProperties = criteria.getCriteria().keySet();
+		for (String property: applianceProperties) {
+			switch (property) {
+				case "price", "batteryCapacity", "displayInches", "memoryROM", "flashMemoryCapacity" -> {
+					if (!criteria.getCriteria().get(property).getClass().getName().contains("Integer")){
+						return true;
+					}
+				}
+				case "color" -> {
+					if (!criteria.getCriteria().get(property).getClass().getName().contains("String")){
+						return true;
+					}
+				}
+				default -> {
+					return false;
+				}
+			}
+		}
+		return false;
+	}
+
+	private static boolean VacuumCleanerValidator(Criteria criteria){
+		Set<String> applianceProperties = criteria.getCriteria().keySet();
+		for (String property: applianceProperties) {
+			switch (property) {
+				case "price", "powerConsumption", "motorSpeedRegulation", "cleaningWidth" -> {
+					if (!criteria.getCriteria().get(property).getClass().getName().contains("Integer")){
+						return true;
+					}
+				}
+				default -> {
+					return false;
+				}
+			}
+		}
+		return false;
+	}
+
+
+
+
+
+}
